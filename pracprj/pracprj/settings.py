0097ac9b5e22470d35d452fb91e7d8d788e24eeb
rishabh-28-shubham <ectopdash@gmail.com>
2025-07-17 22:19:06 +0530

    Models & Admin Panel

--- pracprj/pracprj/settings.py
+++ pracprj/pracprj/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media Folders
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- pracprj/testapp/models.py
+++ pracprj/testapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Ch_Department(models.TextChoices):
+    Human_Resource = 'HR', 'Human Resource',
+    Sales = 'SL', 'Sales',
+    Finance = 'FN', 'Finance',
+    Technical = 'TH', 'Technical'
+
 # Create your models here.
+class Employee(models.Model):
+    Emp_name = models.TextField(max_length=100)
+    Emp_address = models.TextField(max_length=200)
+    Emp_department = models.CharField(max_length=50 , choices=Ch_Department.choices, default = 'Sales')
+    Emp_profile = models.ImageField(upload_to='emp_prf_img/')
